368951c64162bf8aaf77fdab1cbf259251bfc5ae
Chris Woodle <chris@droplit.io>
2017-12-03 05:23:58 -0500

    Initial Commit

---
+++ embedded/embedded.ino
@@ -0,0 +1,72 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            6
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      24
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+void setup() {
+
+  Serial.begin(115200);    // Initialize serial communications with the PC
+  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+  pixels.begin(); // This initializes the NeoPixel library.
+  blink(0, 0, 40, 5, 100);
+}
+
+void loop() {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  Serial.println("card");
+  blink(0, 40, 0, 3, 200);
+  delay(2000);
+
+}
+
+void blink(int r, int g, int b, int count, int delayval) {
+  for (int blinks = 0; blinks < count; blinks++) {
+    for (int i = 0; i < NUMPIXELS; i++) {
+      pixels.setPixelColor(i, pixels.Color(r, g, b)); // Moderately bright green color.
+    }
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval);
+    for (int i = 0; i < NUMPIXELS; i++) {
+      pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Moderately bright green color.
+    }
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval);
+  }
+}
---
+++ index.js
@@ -0,0 +1,16 @@
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+const serial = new SerialPort('COM5', {
+    baudRate: 115200
+});
+
+const parser = serial.pipe(new Readline({ delimiter: '\n' }));
+parser.on('data', function (data) {
+    if (data.toString().includes('card')) {
+        console.log('card scanned!');
+    }
+})
+
+serial.on('open', () => {
+    console.log('serial port opened');
+});
